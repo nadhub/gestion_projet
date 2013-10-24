a16670ecbbe98e0510e7240efe05549dfe775ec2
nadhub <nadir.hamaidi@gmail.com>
2013-10-24 15:25:45 +0200

    Update recetteTest.php

--- tests/units/recetteTest.php
+++ tests/units/recetteTest.php
@@ -2,9 +2,9 @@
 
 namespace tests\units;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once '/../../vendor/autoload.php';
 
-include __DIR__ . '/../../classes/recette.php';
+include '/../../classes/recette.php';
 
 use \mageekguy\atoum;
 use \recette;
